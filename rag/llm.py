78083f3acaa8c4c4716c1e99e25b48f04dd6354a
closeup1202 <closeup1202@gmail.com>
2024-09-04 01:28:58 +0900

    [feat] llm end

--- rag/llm.py
+++ rag/llm.py
@@ -1,18 +1,18 @@
 from dotenv import load_dotenv 
 from langchain_upstage import ChatUpstage, UpstageEmbeddings
 from langchain_pinecone import PineconeVectorStore
-
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
-from langchain.chains.combine_documents import create_stuff_documents_chain
-
-from rag.rag_prompt import get_contextualize_q_prompt, get_qa_prompt
+from rag.rag_prompt import get_retriever_chain, get_dictionary_chain
 
 load_dotenv()
 store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+  if session_id not in store:
+    store[session_id] = ChatMessageHistory()
+  return store[session_id]
 
 def get_llm():
   llm = ChatUpstage()
@@ -26,32 +26,20 @@ def get_retriever(index):
   database = PineconeVectorStore.from_existing_index(index_name=index, embedding=embedding)
   return database.as_retriever()
 
-def get_history_retriever(llm, retriever):
-  return create_history_aware_retriever(llm, retriever, get_contextualize_q_prompt)
-
 def get_rag_chain(llm, retriever):
-  history_aware_retriever = get_history_retriever(llm, retriever)
-  question_answer_chain = create_stuff_documents_chain(llm, get_qa_prompt)
-  rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-
-  conversational_rag_chain = RunnableWithMessageHistory(
-    rag_chain,
+  retriever_chain = get_retriever_chain(llm, retriever)
+  return RunnableWithMessageHistory(
+    retriever_chain,
     get_session_history,
     input_messages_key="input",
     history_messages_key="chat_history",
     output_messages_key="answer",
   ).pick("answer")
 
-  return conversational_rag_chain
-
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-  if session_id not in store:
-    store[session_id] = ChatMessageHistory()
-  return store[session_id]
-
 def get_ai_response(user_question):
   retriever = get_retriever("vtw")
   llm = get_llm()
   rag_chain = get_rag_chain(llm, retriever=retriever)
-  ai_response = rag_chain.stream({"input": user_question}, config={"configurable": {"session_id": "abc123"}},)
-  return ai_response
+  dictionary_chain = get_dictionary_chain(llm)
+  vtw_chain = {"input": dictionary_chain} | rag_chain
+  return vtw_chain.stream({"question": user_question}, config={"configurable": {"session_id": "abc123"}},)
--- rag/rag_prompt.py
+++ rag/rag_prompt.py
@@ -1,23 +1,42 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from rag.config import answer_examples
 
-def get_contextualize_q_system_prompt():
-  return (
-    "Given a chat history and the latest user question "
-    "which might reference context in the chat history, "
-    "formulate a standalone question which can be understood "
-    "without the chat history. Do NOT answer the question, "
-    "just reformulate it if needed and otherwise return it as is."
+def get_history_retriever(llm, retriever):
+  contextualize_q_system_prompt = (
+      "Given a chat history and the latest user question "
+      "which might reference context in the chat history, "
+      "formulate a standalone question which can be understood "
+      "without the chat history. Do NOT answer the question, "
+      "just reformulate it if needed and otherwise return it as is."
   )
 
-def get_contextualize_q_prompt():
-  return ChatPromptTemplate.from_messages(
-    [
-      ("system", get_contextualize_q_system_prompt()),
-      MessagesPlaceholder("chat_history"),
-      ("human", "{input}"),
-    ]
+  contextualize_q_prompt = ChatPromptTemplate.from_messages(
+      [
+          ("system", contextualize_q_system_prompt),
+          MessagesPlaceholder("chat_history"),
+          ("human", "{input}"),
+      ]
   )
+
+  return create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+  )
+
+def get_dictionary_chain(llm):
+  dictionary = ["브띠따 -> VTW"]
+  prompt = ChatPromptTemplate.from_template(f"""
+    사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해 주세요.
+    만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
+    사전 : {dictionary}
+
+    질문 : {{question}}
+  """)
+
+  return prompt | llm | StrOutputParser()
 
 def get_few_shot_prompt(examples):
   example_prompt = ChatPromptTemplate.from_messages(
@@ -32,8 +51,10 @@ def get_few_shot_prompt(examples):
       examples=examples,
   )
 
-def get_system_prompt():
-   return (
+def get_question_answer_chain(llm):
+  few_shot_prompt = get_few_shot_prompt(answer_examples)
+
+  system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
@@ -43,12 +64,18 @@ def get_system_prompt():
     "{context}"
   )
 
-def get_qa_prompt():
-  return ChatPromptTemplate.from_messages(
-    [
-      ("system", get_system_prompt()), 
-      get_few_shot_prompt(answer_examples),
-      MessagesPlaceholder("chat_history"),
-      ("human", "{input}"),
-    ]
+  qa_prompt = ChatPromptTemplate.from_messages(
+      [
+          ("system", system_prompt), 
+          few_shot_prompt,
+          MessagesPlaceholder("chat_history"),
+          ("human", "{input}"),
+      ]
   )
+
+  return create_stuff_documents_chain(llm, qa_prompt)
+
+def get_retriever_chain(llm, retriever):
+  history_aware_retriever = get_history_retriever(llm, retriever)
+  question_answer_chain = get_question_answer_chain(llm)
+  return create_retrieval_chain(history_aware_retriever, question_answer_chain)
